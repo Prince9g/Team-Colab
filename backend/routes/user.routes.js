015158c2e227574da8768b87a7dbe060b3d4863c
akki2332 <akashsoam123456789@gmail.com>
2026-01-28 16:15:01 +0530

    middleware created

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { getTask, Login, Signup,profile} from '../controllers/user.controller.js';
-
+import { auth } from '../middleware/auth.middleware.js';
 const router = express.Router();
 
 router.post('/signup', Signup);
 router.post('/login', Login);
-router.get('/profile',profile)
+router.get('/profile',auth,profile)
 router.get('/getUserTask/:id', getTask);
 
 export default router;

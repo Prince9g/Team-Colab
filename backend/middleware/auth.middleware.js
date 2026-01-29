6f6eaef6a51a04b3cc4decac8f236bdc25ab42b8
Prince9g <142648202+Prince9g@users.noreply.github.com>
2026-01-29 13:05:06 +0530

    access token resolved

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -5,10 +5,11 @@ import User from '../models/user.model.js';
 
 export const auth=async(req,res,next)=>{
         const token=req.cookies.accesstoken;
+        console.log(token);
         if(!token){
           return res.status(401).json({message:"Unauthorized"})
         }
-    try {
+        try {
       const decoded=jwt.verify(token,process.env.access_secret);
       const user=await User.findById(decoded.userid).select("-password");
       req.user=user;
